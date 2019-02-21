1ba8ebff31596ee2ab2fe6f327c8fa4c7ec99aeb
jtertil <jtertil@gmail.com>
2019-02-21 17:35:24 +0100

    initial

---
+++ bookmarks/bookmarks.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+class User:
+    def __init__(self, firstname, lastname):
+        self.firstname = firstname
+        self.lastname = lastname
+
+    # def initials(self):
+    #     return "{}. {}.".format(self.firstname[0], self.lastname[0])
+
+    def __str__ (self):
+        return "{}. {}.".format(self.firstname[0], self.lastname[0])
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/add')
+def add():
+    return render_template('add.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
+
+@app.errorhandler(500)
+def server_error(e):
+    return render_template('500.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
