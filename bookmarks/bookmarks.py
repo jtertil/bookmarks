63dbad18d7990f76e6ee6ba2c4571d83d79c6f68
jtertil <jtertil@gmail.com>
2019-02-22 17:34:20 +0100

    WTF validation

--- bookmarks/bookmarks.py
+++ bookmarks/bookmarks.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect, flash
 from datetime import datetime
+from forms import BookmarkForm
 
 app = Flask(__name__)
 
@@ -16,9 +17,10 @@ class User:
 
 bookmarks = []
 app.config['SECRET_KEY'] = '\x87&X\x1d\x11\xf7\x91\xd6\xd6\xdf\xaa9\xfd\xfc}\x81w\tW[=\xd3\x8a\x89'
-def store_bookmark(url):
+def store_bookmark(url, description):
     bookmarks.append(dict(
     url = url,
+    description = description,
     user = "temp",
     date = datetime.utcnow()
     ))
@@ -33,12 +35,14 @@ def index():
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
-    if request.method == "POST":
-        url = request.form['url']
-        store_bookmark(url)
-        flash("Stored bookmark '{}'".format(url))
+    form = BookmarkForm()
+    if form.validate_on_submit():
+        url = form.url.data
+        description = form.description.data
+        store_bookmark(url, description)
+        flash("Stored '{}'".format(description))
         return redirect(url_for('index'))
-    return render_template('add.html')
+    return render_template('add.html', form=form)
 
 @app.errorhandler(404)
 def page_not_found(e):
