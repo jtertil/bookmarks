495c6ecdeed57193beae88280489f4d90f4673b9
jtertil <jtertil@gmail.com>
2019-02-22 14:56:07 +0100

    add bookmark, flashes

--- bookmarks/bookmarks.py
+++ bookmarks/bookmarks.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect, flash
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -13,13 +14,30 @@ class User:
     def __str__ (self):
         return "{}. {}.".format(self.firstname[0], self.lastname[0])
 
+bookmarks = []
+app.config['SECRET_KEY'] = '\x87&X\x1d\x11\xf7\x91\xd6\xd6\xdf\xaa9\xfd\xfc}\x81w\tW[=\xd3\x8a\x89'
+def store_bookmark(url):
+    bookmarks.append(dict(
+    url = url,
+    user = "temp",
+    date = datetime.utcnow()
+    ))
+
+def new_bookmarks(num):
+    return sorted(bookmarks, key=lambda bm: bm['date'], reverse=True) [:num]
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', new_bookmarks=new_bookmarks(5))
 
-@app.route('/add')
+@app.route('/add', methods=['GET', 'POST'])
 def add():
+    if request.method == "POST":
+        url = request.form['url']
+        store_bookmark(url)
+        flash("Stored bookmark '{}'".format(url))
+        return redirect(url_for('index'))
     return render_template('add.html')
 
 @app.errorhandler(404)
