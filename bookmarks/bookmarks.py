310c8541b7d4fed6840aa0d23ce7a45b14f60269
jtertil <jtertil@gmail.com>
2019-02-26 18:31:35 +0100

    db-preaparation

--- bookmarks/bookmarks.py
+++ bookmarks/bookmarks.py
@@ -1,37 +1,52 @@
 from flask import Flask, render_template, url_for, request, redirect, flash
-from datetime import datetime
-from forms import BookmarkForm
-
 app = Flask(__name__)
-
-class User:
-    def __init__(self, firstname, lastname):
-        self.firstname = firstname
-        self.lastname = lastname
-
-    # def initials(self):
-    #     return "{}. {}.".format(self.firstname[0], self.lastname[0])
-
-    def __str__ (self):
-        return "{}. {}.".format(self.firstname[0], self.lastname[0])
-
-bookmarks = []
 app.config['SECRET_KEY'] = '\x87&X\x1d\x11\xf7\x91\xd6\xd6\xdf\xaa9\xfd\xfc}\x81w\tW[=\xd3\x8a\x89'
-def store_bookmark(url, description):
-    bookmarks.append(dict(
-    url = url,
-    description = description,
-    user = "temp",
-    date = datetime.utcnow()
-    ))
 
-def new_bookmarks(num):
-    return sorted(bookmarks, key=lambda bm: bm['date'], reverse=True) [:num]
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+from flask_sqlalchemy import SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'bookmarks.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# from datetime import datetime
+from forms import BookmarkForm
+import models
+
+
+
+
+
+# bookmarks = []
+# def store_bookmark(url, description):
+#     bookmarks.append(dict(
+#     url = url,
+#     description = description,
+#     user = "temp",
+#     date = datetime.utcnow()
+#     ))
+
+# def new_bookmarks(num):
+#     return []
+
+
+    # class User:
+    #     def __init__(self, firstname, lastname):
+    #         self.firstname = firstname
+    #         self.lastname = lastname
+    #
+    #     # def initials(self):
+    #     #     return "{}. {}.".format(self.firstname[0], self.lastname[0])
+    #
+    #     def __str__ (self):
+    #         return "{}. {}.".format(self.firstname[0], self.lastname[0])
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', new_bookmarks=new_bookmarks(5))
+    return render_template('index.html', new_bookmarks=models.Bookmark.newest(5))
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
@@ -39,7 +54,10 @@ def add():
     if form.validate_on_submit():
         url = form.url.data
         description = form.description.data
-        store_bookmark(url, description)
+        # store_bookmark(url, description)
+        bm = models.Bookmark(url=url, description=description)
+        db.session.add(bm)
+        db.session.commit()
         flash("Stored '{}'".format(description))
         return redirect(url_for('index'))
     return render_template('add.html', form=form)
---
+++ bookmarks/manage.py
@@ -0,0 +1,18 @@
+from bookmarks import app, db
+from flask_script import Manager, prompt_bool
+
+manager = Manager(app)
+
+@manager.command
+def initdb():
+    db.create_all()
+    print('Initialized the database')
+
+@manager.command
+def dropdb():
+    if prompt_bool("Are you sure you want lose all your data"):
+        db.drop_all()
+        print('Dropped database')
+
+if __name__ == "__main__":
+    manager.run()
