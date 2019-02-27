4f84f12181d6731980fa60ab6f4380bdd239d814
jtertil <jtertil@gmail.com>
2019-02-27 14:44:05 +0100

    db-runup

--- bookmarks/bookmarks.py
+++ bookmarks/bookmarks.py
@@ -14,34 +14,9 @@ db = SQLAlchemy(app)
 from forms import BookmarkForm
 import models
 
-
-
-
-
-# bookmarks = []
-# def store_bookmark(url, description):
-#     bookmarks.append(dict(
-#     url = url,
-#     description = description,
-#     user = "temp",
-#     date = datetime.utcnow()
-#     ))
-
-# def new_bookmarks(num):
-#     return []
-
-
-    # class User:
-    #     def __init__(self, firstname, lastname):
-    #         self.firstname = firstname
-    #         self.lastname = lastname
-    #
-    #     # def initials(self):
-    #     #     return "{}. {}.".format(self.firstname[0], self.lastname[0])
-    #
-    #     def __str__ (self):
-    #         return "{}. {}.".format(self.firstname[0], self.lastname[0])
-
+# fake login
+def logged_in_user():
+    return models.User.query.filter_by(username='developer').first()
 
 @app.route('/')
 @app.route('/index')
@@ -53,9 +28,11 @@ def add():
     form = BookmarkForm()
     if form.validate_on_submit():
         url = form.url.data
-        description = form.description.data
-        # store_bookmark(url, description)
-        bm = models.Bookmark(url=url, description=description)
+        if form.description.data == "":
+            description = url
+        else:
+            description = form.description.data
+        bm = models.Bookmark(user=logged_in_user(), url=url, description=description)
         db.session.add(bm)
         db.session.commit()
         flash("Stored '{}'".format(description))
@@ -71,5 +48,5 @@ def server_error(e):
     return render_template('500.html')
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
--- bookmarks/manage.py
+++ bookmarks/manage.py
@@ -1,11 +1,16 @@
 from bookmarks import app, db
 from flask_script import Manager, prompt_bool
 
+from models import User
+
 manager = Manager(app)
 
 @manager.command
 def initdb():
     db.create_all()
+    db.session.add(User(username="developer", email="some@email.com"))
+    db.session.add(User(username="developer2", email="some2@email.com"))
+    db.session.commit()
     print('Initialized the database')
 
 @manager.command
